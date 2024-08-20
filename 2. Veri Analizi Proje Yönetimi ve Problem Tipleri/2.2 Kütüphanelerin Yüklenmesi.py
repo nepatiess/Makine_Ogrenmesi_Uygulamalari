ade7bfb69ad55d21bb3e6d337dc21525a5880f15
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-20 18:42:50 +0300

    Add files via upload

---
+++ 2. Veri Analizi Proje Yönetimi ve Problem Tipleri/2.2 Kütüphanelerin Yüklenmesi.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 17:04:56 2024
+
+@author: zeynep
+"""
+
+#2.2 Kütüphanelerin yüklenmesi
+
+#import bölümü
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#kod bölümü
