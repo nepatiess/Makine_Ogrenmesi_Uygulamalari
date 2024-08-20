60a35867cf59f75048eeeba84ed0fdf548491636
Zeynep Koz <128158220+nepatiess@users.noreply.github.com>
2024-08-20 17:09:29 +0300

    Add files via upload

---
+++ 1. Hoş Geldiniz!/1.3 Python ve Anaconda, Spyder.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 17:04:56 2024
+
+@author: zeynep
+"""
+
+
+
+print("merhaba udemy")
+# yorum satırı
+print("ikinci merhaba")
+x = 10
+y = [1,2,4]
+print(x)
+
